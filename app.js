4f1e91393b4f9ce685a4595d0e419e4fe3e0fcda
Yan MATAGNE <ymatagne@norsys.fr>
2017-10-31 14:24:21 -0400

    init repo

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const SSE = require('express-sse');
+const sse = new SSE(["array", "containing", "initial", "content", "(optional)"]);
+ 
+const app = express()
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+app.get('/bixi', sse.init);
+
+app.post('/api/bixi', function (req, res) {
+	sse.send(req.body);
+	res.send('ok');
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
